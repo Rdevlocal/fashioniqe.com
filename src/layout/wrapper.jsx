d955f3c53f2ecb537a3aa0ff3a0550484dae25da
Kareltheketa <102698582+Kareltheketa@users.noreply.github.com>
2024-12-16 22:54:43 +0100

    Update wrapper.jsx

--- src/layout/wrapper.jsx
+++ src/layout/wrapper.jsx
@@ -2,10 +2,10 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
-// internal
+// internal imports
 import BackToTopCom from "@/components/common/back-to-top";
 import ProductModal from "@/components/common/product-modal";
-import {  initialOrderQuantity,};
+import { initialOrderQuantity } from "@/redux/features/orderSlice";
 import { get_wishlist_products } from "@/redux/features/wishlist-slice";
 import { get_compare_products } from "@/redux/features/compareSlice";
 import useAuthCheck from "@/hooks/use-auth-check";
@@ -17,26 +17,32 @@ const Wrapper = ({ children }) => {
   const authChecked = useAuthCheck();
 
   useEffect(() => {
-    dispatch(get_wishlist_products());
-    dispatch(get_compare_products());
-    dispatch(initialOrderQuantity());
+    try {
+      dispatch(get_wishlist_products());
+      dispatch(get_compare_products());
+      dispatch(initialOrderQuantity());
+    } catch (error) {
+      console.error("An error occurred while dispatching actions: ", error);
+    }
   }, [dispatch]);
 
-  return !authChecked ? (
-    <div
-      className="d-flex align-items-center justify-content-center"
-      style={{ height: "100vh" }}
-    >
-      <Loader spinner="fade" loading={!authChecked} />
-    </div>
-  ) : (
+  if (!authChecked) {
+    return (
+      <div
+        className="d-flex align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <Loader spinner="fade" loading={!authChecked} />
+      </div>
+    );
+  }
+
+  return (
     <div id="wrapper">
       {children}
       <BackToTopCom />
       <ToastContainer />
-      {/* product modal start */}
       {productItem && <ProductModal />}
-      {/* product modal end */}
     </div>
   );
 };
